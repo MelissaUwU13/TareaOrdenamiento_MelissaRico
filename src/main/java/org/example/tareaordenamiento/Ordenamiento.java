0183b69a55f61816bf48f68a8e1e236c09a13361
MelissaUwU13 <melissa.rico@uabc.edu.mx>
2026-05-01 21:01:02 -0700

    actualizacion detalles menores

--- src/main/java/org/example/tareaordenamiento/Ordenamiento.java
+++ src/main/java/org/example/tareaordenamiento/Ordenamiento.java
@@ -5,8 +5,11 @@ import java.util.*;
 
 public class Ordenamiento {
 
-    private static final int MAX_CHUNK_SIZE = 100; // pequeño para pruebas
+    //prueba
+    private static final int MAX_CHUNK_SIZE = 100;
 
+    //En esta parte modificaciones nuestro archivo de registros
+    //para poder acomodarlo para la particion y ordenamiento
     static class Registro {
         String nombre;
         int id;
@@ -18,9 +21,9 @@ public class Ordenamiento {
             this.pais = pais;
         }
 
-        public static Registro fromLine(String line) {
-            String[] parts = line.split(",");
-            return new Registro(parts[0], Integer.parseInt(parts[1]), parts[2]);
+        public static Registro Partes(String linea) {
+            String[] partes = linea.split(",");
+            return new Registro(partes[0], Integer.parseInt(partes[1]), partes[2]);
         }
 
         @Override
@@ -29,104 +32,119 @@ public class Ordenamiento {
         }
     }
 
+    //Nodo para la fase 2 de Merge
     static class MergeNode {
         Registro registro;
-        int fileIndex;
+        int id;
 
-        public MergeNode(Registro registro, int fileIndex) {
+        public MergeNode(Registro registro, int id) {
             this.registro = registro;
-            this.fileIndex = fileIndex;
+            this.id = id;
         }
     }
 
-    public static List<File> partitionAndSort(File inputFile) throws IOException {
-        List<File> tempFiles = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+    //En este metodo creamos los chunks, es decir creamos el archivo y pasamos la informacion al archivo
+    private static File crearChunk(List<Registro> buffer, int index) throws IOException {
+        buffer.sort(Comparator.comparingInt(r -> r.id));
 
+        File archivoTemp = new File("temp_" + index + ".dat");
+        BufferedWriter texto = new BufferedWriter(new FileWriter(archivoTemp));
+
+        //ingresamos la informacion al archivo al nuevo archivoTemporal
+        for (Registro r : buffer) {
+            texto.write(r.toString());
+            texto.newLine();
+        }
+
+        //cerramos el archivo y regresamos el archivo temporal ya creado
+        texto.close();
+        return archivoTemp;
+    }
+
+
+    //Fase 1 - Particion
+    public static List<File> Particion_Ordenamiento(File archivo) throws IOException {
+        List<File> archivosTemp = new ArrayList<>();
+        BufferedReader lectores = new BufferedReader(new FileReader(archivo));
         List<Registro> buffer = new ArrayList<>();
-        String line;
-        int currentSize = 0;
-        int fileCount = 0;
 
-        while ((line = reader.readLine()) != null) {
-            Registro r = Registro.fromLine(line);
+        String linea;
+        int tamaño = 0;
+        int contador = 0;
+
+        //Estara en ciclo while mientras el archivo tenga contenido dentro
+        while ((linea = lectores.readLine()) != null) {
+            Registro r = Registro.Partes(linea);
             buffer.add(r);
-            currentSize += line.length();
+            tamaño += linea.length();
 
-            if (currentSize >= MAX_CHUNK_SIZE) {
-                tempFiles.add(writeChunk(buffer, fileCount++));
+            //si el tamaño es igual o menor al maximo de chunk entonces se agregan al archivo temporal actual
+            if (tamaño >= MAX_CHUNK_SIZE) {
+                archivosTemp.add(crearChunk(buffer, contador++));
                 buffer.clear();
-                currentSize = 0;
+                tamaño = 0;
             }
         }
 
         if (!buffer.isEmpty()) {
-            tempFiles.add(writeChunk(buffer, fileCount++));
+            archivosTemp.add(crearChunk(buffer, contador++));
         }
 
-        reader.close();
-        return tempFiles;
+        //cerramos el archivo y regresamos el archivo temporal ya creado
+        lectores.close();
+        return archivosTemp;
     }
 
-    private static File writeChunk(List<Registro> buffer, int index) throws IOException {
-        buffer.sort(Comparator.comparingInt(r -> r.id));
+    //Fase 2 - Mezcla (Merge)
+    public static void mergeFiles(List<File> archivosTemp, File archivoSalida) throws IOException {
 
-        File tempFile = new File("temp_" + index + ".dat");
-        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-
-        for (Registro r : buffer) {
-            writer.write(r.toString());
-            writer.newLine();
-        }
-
-        writer.close();
-        return tempFile;
-    }
-
-    public static void mergeFiles(List<File> tempFiles, File outputFile) throws IOException {
-
-        List<BufferedReader> readers = new ArrayList<>();
+        List<BufferedReader> lectores = new ArrayList<>();
         PriorityQueue<MergeNode> minHeap =
                 new PriorityQueue<>(Comparator.comparingInt(n -> n.registro.id));
 
-        for (int i = 0; i < tempFiles.size(); i++) {
-            BufferedReader br = new BufferedReader(new FileReader(tempFiles.get(i)));
-            readers.add(br);
+        //Preparamos los archivos, obtenemos los primeros datos y los agregamos a minHeap para despues ejecutar el merge
+        for (int i = 0; i < archivosTemp.size(); i++) {
+            BufferedReader br = new BufferedReader(new FileReader(archivosTemp.get(i)));
+            lectores.add(br);
 
-            String line = br.readLine();
-            if (line != null) {
-                minHeap.add(new MergeNode(Registro.fromLine(line), i));
+            String linea = br.readLine();
+            if (linea != null) {
+                minHeap.add(new MergeNode(Registro.Partes(linea), i));
             }
         }
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
 
+        BufferedWriter texto = new BufferedWriter(new FileWriter(archivoSalida));
+
+        //Agregamos el texto al archivo de texto de salida hasta que el contenido de la mezcla este vacio
         while (!minHeap.isEmpty()) {
             MergeNode node = minHeap.poll();
-            writer.write(node.registro.toString());
-            writer.newLine();
+            texto.write(node.registro.toString());
+            texto.newLine();
 
-            BufferedReader br = readers.get(node.fileIndex);
-            String line = br.readLine();
+            BufferedReader br = lectores.get(node.id);
+            String linea = br.readLine();
 
-            if (line != null) {
-                minHeap.add(new MergeNode(Registro.fromLine(line), node.fileIndex));
+            if (linea != null) {
+                minHeap.add(new MergeNode(Registro.Partes(linea), node.id));
             }
         }
 
-        writer.close();
+        texto.close();
 
-        for (BufferedReader br : readers) {
+        //Cierra los archivos
+        for (BufferedReader br : lectores) {
             br.close();
         }
     }
 
+    //Main para probar el codigo
     public static void main(String[] args) throws IOException {
         File archivo1 = new File("archivo.dat");
         File archivo2 = new File("archivoOrdenado.dat");
 
-        List<File> tempFiles = partitionAndSort(archivo1);
-        mergeFiles(tempFiles, archivo2);
+        List<File> archivosTemp = Particion_Ordenamiento(archivo1);
+        mergeFiles(archivosTemp, archivo2);
 
     }
 }
